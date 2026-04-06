e086bc75b366a36291373c4b0e75513d395ebf02
TitoT-00 <thomastito88@gmail.com>
2026-04-06 17:18:06 -0400

    server update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Test CI/CD ");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("App is running on AWS");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
